540d1dc2789517287a79f295894dfd8d283078e1
Zeon <zeon@zeon.hu>
2020-11-05 14:12:34 +0000

    admin JS

---
+++ js/admin.js
@@ -0,0 +1,36 @@
+// JS for the forms' settings page
+window.addEventListener('DOMContentLoaded', function(){
+
+	var submitSettings = function() {
+		console.log($('#uploadlogo').prop('files')[0]);
+
+		$.ajax({
+			url: OC.generateUrl('/apps/forms/settings'),
+			type: 'POST',
+			data: $('#forms_settings').serialize(),
+			success: function(){
+				OC.msg.finishedSuccess('#forms_settings_msg', t('forms', 'Saved'));
+			},
+			error: function(xhr){
+				OC.msg.finishedError('#forms_settings_msg', xhr.responseJSON);
+			}
+		});
+	};
+
+	$('#uploadlogo').on('change', function () {
+		var fileReader = new FileReader();
+		fileReader.onload = function () { $('#uploadLogoData').val(fileReader.result); };
+		fileReader.readAsDataURL($('#uploadlogo').prop('files')[0]);
+	});
+
+	var setupCurtain = function () {
+		if ($('#enable-access').prop('checked'))
+			$('.forms-access').removeClass('forms-access-curtain');
+		else
+			$('.forms-access').addClass('forms-access-curtain');
+	};
+
+	$('#forms_settings_submit').click(submitSettings);
+	$('#enable-access').change(setupCurtain);
+	setupCurtain();
+});
